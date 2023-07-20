661274c3d7d7fda318172ee00d2fa76e7506bda8
KurenkievTimur <ti-mur99@mail.ru>
2023-07-20 18:36:17 +0600

    add validation

--- src/main/java/com/kurenkievtimur/Main.java
+++ src/main/java/com/kurenkievtimur/Main.java
@@ -1,5 +1,6 @@
 package com.kurenkievtimur;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,14 +11,64 @@ public class Main {
     public static void printMenu() {
         Scanner scanner = new Scanner(System.in);
 
+        inputTrafficLight(scanner);
+        choiceMenu(scanner);
+
+        System.out.println("Bye!");
+    }
+
+    public static void inputTrafficLight(Scanner scanner) {
         System.out.println("Welcome to the traffic management system!");
 
         System.out.print("Input the number of roads: ");
-        int roads = scanner.nextInt();
+        int roads;
+
+        boolean isValidRoads = false;
+        while (!isValidRoads) {
+            String input = scanner.nextLine();
+            if (!isNumber(input)) {
+                System.out.print("Error! Incorrect Input. Try again: ");
+                continue;
+            }
+
+            roads = Integer.parseInt(input);
+
+            isValidRoads = roads > 0;
+            if (!isValidRoads)
+                System.out.print("Error! Incorrect Input. Try again: ");
+        }
 
         System.out.print("Input the interval: ");
-        int interval = scanner.nextInt();
+        int interval;
 
+        boolean isValidInterval = false;
+        while (!isValidInterval) {
+            String input = scanner.nextLine();
+            if (!isNumber(input)) {
+                System.out.print("Error! Incorrect Input. Try again: ");
+                continue;
+            }
+
+            interval = Integer.parseInt(input);
+
+            isValidInterval = interval > 0;
+            if (!isValidInterval)
+                System.out.print("Error! Incorrect Input. Try again: ");
+        }
+
+        clearConsole();
+    }
+
+    public static boolean isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void choiceMenu(Scanner scanner) {
         boolean isExit = false;
         while (!isExit) {
             System.out.println("Menu:");
@@ -26,14 +77,32 @@ public class Main {
             System.out.println("3. System");
             System.out.println("0. Quit");
 
-            switch (scanner.nextInt()) {
-                case 1 -> System.out.println("Road added");
-                case 2 -> System.out.println("Road deleted");
-                case 3 -> System.out.println("System opened");
-                case 0 -> isExit = true;
+            switch (scanner.nextLine()) {
+                case "1" -> System.out.println("Road added");
+                case "2" -> System.out.println("Road deleted");
+                case "3" -> System.out.println("System opened");
+                case "0" -> isExit = true;
+                default -> {
+                    System.out.println("Incorrect option");
+                    scanner.nextLine();
+                    clearConsole();
+                    continue;
+                }
+            }
+
+            if (!isExit) {
+                scanner.nextLine();
+                clearConsole();
             }
         }
+    }
 
-        System.out.println("Bye!");
+    public static void clearConsole() {
+        try {
+            var clearCommand = System.getProperty("os.name").contains("Windows")
+                    ? new ProcessBuilder("cmd", "/c", "cls")
+                    : new ProcessBuilder("clear");
+            clearCommand.inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException ignored) {}
     }
 }
